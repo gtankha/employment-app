86c477dba19364998dff78493f0291e8a35ba174
gtankha <46304828+gtankha@users.noreply.github.com>
2020-10-30 22:26:20 -0700

    added skills

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -3,10 +3,12 @@ const router = require('express').Router();
 const userRoutes = require('./user-routes');
 const jobRoutes = require('./job-routes');
 const interestRoutes = require('./interest-routes');
+const skillRoutes = require('./skill-routes');
 
 router.use('/interests', interestRoutes);
 
 router.use('/users', userRoutes);
 router.use('/jobs', jobRoutes);
+router.use('/skills', skillRoutes);
 
 module.exports = router;
---
+++ controllers/api/skill-routes.js
@@ -0,0 +1,137 @@
+const router = require('express').Router();
+const { Skills,Users,userSkills,jobSkills, Jobs} = require("../../models");
+// const userSkills = require('../../models/User-skills');
+// const Users = require('../../models/Users');
+
+// GET /api/users
+router.get('/', (req, res) => {
+
+
+    Skills.findAll({
+        include: [
+            {
+            
+         model: Users,
+         attributes: ['full_name','company_name','description','email','type'],
+         through: userSkills
+        // as: 'user_skills'
+            },
+            {
+          model: Jobs,
+          attributes: ['title','description','company_id'],
+          through: jobSkills
+        //  as: 'job_skills'     
+            }
+        ]
+    })
+        .then(dbUserData => res.json(dbUserData))
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+
+router.get('/:id', (req, res) => {
+    Skills.findOne({
+        where: {
+            id: req.params.id
+        },
+        include: [
+            {
+            
+         model: Users,
+         attributes: ['full_name','company_name','description','email','type'],
+         through: userSkills
+     //    as: 'user_skills'
+            },
+            {
+          model: Jobs,
+          attributes: ['title','description','company_id'],
+          through: jobSkills
+       //   as: 'job_skills'     
+            }
+        ]
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No skill found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+});
+
+// POST /api/users
+router.post('/', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+    Skills.create({
+        name: req.body.name,
+    })
+        .then(dbUserData => {
+            res.status(200).json(dbUserData);
+            console.log(dbUserData)
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// PUT /api/users/1
+router.put('/:id', (req, res) => {
+    // expects {username: 'Lernantino', email: 'lernantino@gmail.com', password: 'password1234'}
+
+    // if req.body has exact key/value pairs to match the model, you can just use `req.body` instead
+    Skills.update(
+        {
+            name: req.body.name,
+        }
+        , {
+            where: {
+                id: req.params.id
+            }
+        })
+        .then(dbUserData => {
+            if (!dbUserData[0]) {
+                res.status(404).json({ message: 'No skill found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+
+
+});
+
+// DELETE /api/users/1
+router.delete('/:id', (req, res) => {
+
+  Skills.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(dbUserData => {
+            if (!dbUserData) {
+                res.status(404).json({ message: 'No user found with this id' });
+                return;
+            }
+            res.json(dbUserData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+module.exports = router;
