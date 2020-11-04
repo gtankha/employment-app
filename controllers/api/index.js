fba46e6408ccea4a746ee18a1b26e65226a882e8
gtankha <46304828+gtankha@users.noreply.github.com>
2020-11-03 19:57:34 -0800

    skill routes to get matching skills

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,11 +4,13 @@ const userRoutes = require('./user-routes');
 const jobRoutes = require('./job-routes');
 const interestRoutes = require('./interest-routes');
 const skillRoutes = require('./skill-routes');
+const jobuserskillRoutes = require('./job-user-skill');
 
 router.use('/interests', interestRoutes);
 
 router.use('/users', userRoutes);
 router.use('/jobs', jobRoutes);
 router.use('/skills', skillRoutes);
+router.use('/jobuserskills', jobuserskillRoutes);
 
 module.exports = router;
