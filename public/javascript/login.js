b2932832d9ece72ab1b595c74ff0a482b55eb7df
Marco Evangelista <marcobjj@users.noreply.github.com>
2020-11-07 00:28:03 -0800

    ui enhancements

--- public/javascript/login.js
+++ public/javascript/login.js
@@ -1,3 +1,7 @@
+const signupForm = document.querySelector('#signup-form');
+const loginForm = document.querySelector('#login-form');
+
+
 async function signupFormHandler(event) {
     event.preventDefault();
 
@@ -57,5 +61,34 @@ async function loginFormHandler(event) {
     }
 }
 
+function switchHandler(event)
+{
+    if(!event) signupForm.style.display = 'none';
+
+    event.preventDefault();
+  
+
+    switch (event.target.getAttribute("id")) {
+
+            case "signup-instead":
+                loginForm.style.display = 'none';
+                signupForm.style.display = '';
+                break;
+
+            case "login-instead":
+                loginForm.style.display = '';
+                signupForm.style.display = 'none';
+                break;
+    }
+}
+
+
+
+
 document.querySelector('.signup-form').addEventListener('submit', signupFormHandler);
 document.querySelector('.login-form').addEventListener('submit', loginFormHandler);
+document.querySelector('#signup-instead').addEventListener('click', switchHandler);
+document.querySelector('#login-instead').addEventListener('click', switchHandler);
+
+switchHandler(null);
+
